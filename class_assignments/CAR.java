76ab1bd35bc2efa59e139923982dd7d3d26f29e9
Abhinav04269 <tatoluabhinav3@gmail.com>
2025-07-21 21:51:30 +0530

    CAR

---
+++ class_assignments/CAR.java
@@ -0,0 +1,29 @@
+public class CAR{
+    public static void main(String[] args) {
+
+        String Engine_Type = "AUDI";
+        String Displacement = "4000 cc";
+        String Max_Power = "125405115rpm";
+        String Max_Torque = "4000-5000rpm";
+        byte Num_of_Cylinders = 8;
+        byte Valves_per_Cylinder = 6;
+        String Fuel_Supply_System = "CRDi";
+        boolean Turbo_Charger = true;
+        String Transmission_Type = "Manual";
+        String Gearbox = "6-Speed";
+        String Drive_Type = "RWD";
+
+        System.out.println("Engine & Transmission");
+        System.out.println("Engine Type : " +Engine_Type);
+        System.out.println("Displacement : " +Displacement);
+        System.out.println("Max Power : " +Max_Power);
+        System.out.println("Max Torque : " +Max_Torque);
+        System.out.println("Num of Cylinders : " +Num_of_Cylinders);
+        System.out.println("Valves per Cylinder : " +Valves_per_Cylinder);
+        System.out.println("Fuel Supply System : " +Fuel_Supply_System);
+        System.out.println("Turbo Charger : " +Turbo_Charger);
+        System.out.println("Transmission Type : " +Transmission_Type);
+        System.out.println("Gearbox : " +Gearbox);
+        System.out.println("Drive Type : " +Drive_Type);
+    }
+}
